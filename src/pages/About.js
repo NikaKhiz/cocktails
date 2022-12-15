c382082d2102772756969b323904eacaaf8669f6
NikaKhiz <khizambarelinika123@gmail.com>
2022-12-15 17:05:54 +0400

    Add react router.link pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores dolorem tempore, ratione totam sed pariatur officiis molestias earum rerum nulla fugiat repellat numquam magnam veniam veritatis ut architecto esse minus doloremque porro aliquam nesciunt libero. Accusantium, distinctio sapiente quas inventore explicabo nesciunt optio ea sunt nobis esse beatae quidem amet?</p>
+    </section>
+  );
+};
 
-export default About
+export default About;
