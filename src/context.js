31953a1c3b2d2bd6a5d6bd613284a90836f19721
NikaKhiz <khizambarelinika123@gmail.com>
2022-12-16 23:48:49 +0400

    modifie context. set initial variables . fetch and render the data

--- src/context.js
+++ src/context.js
@@ -1,15 +1,60 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { useCallback } from 'react'
+import React, { useState, useContext, useEffect } from "react";
+import { useCallback } from "react";
 
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
-const AppContext = React.createContext()
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("a");
+  const [cocktails, setCocktails] = useState([]);
+
+  const fetchCocktails = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${search}`);
+      const data = await response.json();
+      const { drinks } = data;
+      if (drinks) {
+        const newCocktails = drinks.map((cocktail) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholoc, strGlass } =
+            cocktail;
+          return {
+            id: idDrink,
+            name: strDrink,
+            img: strDrinkThumb,
+            info: strAlcoholoc,
+            glass: strGlass,
+          };
+        });
+        setCocktails(newCocktails);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchCocktails();
+  }, [search]);
+  return (
+    <AppContext.Provider
+      value={{
+        loading,
+        cocktails,
+        setSearch,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
